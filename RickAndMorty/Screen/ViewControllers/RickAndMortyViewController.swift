96acd5204c2dbd52fb4eaa884468da975dafced6
Mert KARAKAS <mert.karakas@veripark.com>
2021-02-07 00:06:08 +0300

    Project created.

---
+++ RickAndMorty/Screen/ViewControllers/RickAndMortyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RickAndMortyViewController.swift
+//  RickAndMorty
+//
+//  Created by Mert KARAKAŞ on 6.02.2021.
+//
+
+import UIKit
+
+class RickAndMortyViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
